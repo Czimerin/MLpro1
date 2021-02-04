71a645a2f0c2e8ec6687f4efa03f39ceb570742e
Nicholas Kogovsek <nick.kogovek@gmail.com>
2021-02-04 13:25:14 -0500

    Add Documentation

--- redRedWine.py
+++ redRedWine.py
@@ -13,28 +13,29 @@ import numpy as np
 import matplotlib
 
 
-
+#reads in the csv files 
 redWine = pd.read_csv("red.csv")
 whiteWine = pd.read_csv("white.csv")
 
-
+#adds a color column with the vaule R
 redWine["color"] = "R"
+#adds a color column with the vaule W
 whiteWine["color"] ="W"
 
-
+#adds both dataframes into one dataframe
 bothRnW=pd.concat([redWine, whiteWine],axis=0)
 
-bothRnW.head()
-
-
+#renames the columns with a "_" for the space inbetween two words
 redWine.rename(columns={'fixed acidity': 'fixed_acidity','citric acid':'citric_acid','volatile acidity':'volatile_acidity','residual sugar':'residual_sugar','free sulfur dioxide':'free_sulfur_dioxide','total sulfur dioxide':'total_sulfur_dioxide'}, inplace=True)
 whiteWine.rename(columns={'fixed acidity': 'fixed_acidity','citric acid':'citric_acid','volatile acidity':'volatile_acidity','residual sugar':'residual_sugar','free sulfur dioxide':'free_sulfur_dioxide','total sulfur dioxide':'total_sulfur_dioxide'}, inplace=True)
 bothRnW.rename(columns={'fixed acidity': 'fixed_acidity','citric acid':'citric_acid','volatile acidity':'volatile_acidity','residual sugar':'residual_sugar','free sulfur dioxide':'free_sulfur_dioxide','total sulfur dioxide':'total_sulfur_dioxide'}, inplace=True)
 
-
+#takes the first 12 columns of data for trainig
 data = bothRnW.iloc[:,[0,1,2,3,4,5,6,7,8,9,10,11]]
+#takes the last column color for trainig
 labels = bothRnW.iloc[:,[12]]
 
+#Split the data into randow train and test subsets
 xTrain, xTest, yTrain, yTest = train_test_split(data,labels, test_size=0.4, random_state=1 )
 
 knn = KNeighborsClassifier(n_neighbors=3)
